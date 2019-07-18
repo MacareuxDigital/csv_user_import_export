0cae5fe5502f3a74cb8113f830da005b991dba8d
Md Biplob Hossain <biplob.ice@gmail.com>
2019-07-18 20:58:57 +0900

    Fix user import with better mapping selection

--- csv_user_import_export/controllers/single_page/dashboard/system/backup/import_user_csv.php
+++ csv_user_import_export/controllers/single_page/dashboard/system/backup/import_user_csv.php
@@ -1,56 +1,60 @@
 <?php
 namespace  Concrete\Package\CsvUserImportExport\Controller\SinglePage\Dashboard\System\Backup;
 
-
+use Concrete\Core\Attribute\Key\Key;
 use Concrete\Core\File\File;
+use Concrete\Core\File\Version;
+use Concrete\Core\Page\Controller\DashboardPageController;
+use Concrete\Core\User\RegistrationService;
 use Concrete\Core\User\User;
+use Group;
 use League\Csv\Reader;
-use Core;
 use UserAttributeKey;
+use UserInfo;
 
-class ImportUserCsv extends \Concrete\Core\Page\Controller\DashboardPageController
+class ImportUserCsv extends DashboardPageController
 {
-    public function view () {
-
+    public function view()
+    {
     }
 
     public function select_mapping()
     {
+        // CSRF token validation
         if (!$this->token->validate('select_mapping')) {
             $this->error->add($this->token->getErrorMessage());
         }
-        $fID = $this->request->request->get('csv');
+
+        // Get the csv file
+        $fID = $this->request('csv');
         /** @var File|Version|\Concrete\Core\Entity\File\File $f */
-        $f = File::getByID($fID);
-        if (!is_object($f)) {
+        $file = File::getByID($fID);
+        $csvHeader = null;
+        if ($file) {
+            // Validate the csv file
+            $csv = $this->getCsvReader($file);
+            $csv->setHeaderOffset(0);
+            $csvHeader = array_map('trim', $csv->getHeader());
+        }
+
+        if (!is_array($csvHeader)) {
             $this->error->add(t('Invalid file.'));
-        } else {
-            ini_set("auto_detect_line_endings", true);
-
-            $resource = $f->getFileResource();
-            $reader = Reader::createFromStream($resource->readStream());
-            $header = $reader->fetchOne();
-
-            if (!is_array($header)) {
-                $this->error->add(t('Invalid file.'));
-            }
         }
 
         if (!$this->error->has()) {
-            $this->set('f', $f);
-            array_unshift($header, 'Ignore');
-            $this->set('header', $header);
-
-            $columns = $this->getColumns();
-            $this->set('columns', $columns);
-        } else {
-            $this->view();
+            $csvHeader = array_merge([0 => 'Select..'], array_combine($csvHeader, $csvHeader));
+            $headers = iterator_to_array($this->getHeaders());
         }
+
+        $this->set('fID', $fID);
+        $this->set('file', $file);
+        $this->set('csvHeader', $csvHeader);
+        $this->set('headers', $headers);
     }
 
-    public function import($fID = null)
+    public function csv_import($fID = null)
     {
-        if (!$this->token->validate('import')) {
+        if (!$this->token->validate('csv_import')) {
             $this->error->add($this->token->getErrorMessage());
         }
 
@@ -59,70 +63,65 @@ class ImportUserCsv extends \Concrete\Core\Page\Controller\DashboardPageControll
         if (!is_object($f)) {
             $this->error->add(t('Invalid file.'));
         } else {
-            ini_set("auto_detect_line_endings", true);
-            $resource = $f->getFileResource();
-            $reader = Reader::createFromStream($resource->readStream());
-            $header = $reader->fetchOne();
-            if (!is_array($header)) {
+            $csv = $this->getCsvReader($f);
+            $csv->setHeaderOffset(0);
+            $csvHeader = array_map('trim', $csv->getHeader());
+            if (!is_array($csvHeader)) {
                 $this->error->add(t('Invalid file.'));
             }
         }
 
         if (!$this->error->has()) {
             set_time_limit(300);
-            $mapping = $this->request->request->get('mapping');
-            $reader->setOffset(1);
-            $results = $reader->fetch();
-            $columns = $this->getColumns();
+            $records = $csv->getRecords($csvHeader);
+            $headers = iterator_to_array($this->getHeaders());
 
-            foreach ($results as $key => $result) {
-
-                foreach ($columns as $field => $column) {
-                    $$field = '';
-                    if ($this->post($field)) {
-                        $$field = $result[$this->post($field)-1];
+            foreach ($records as $record) {
+                foreach ($headers as $handle => $name) {
+                    $$handle = '';
+                    if ($this->post($handle)) {
+                        $$handle = $record[$this->request($handle)];
                     }
                 }
 
                 // Skip, if email is empty
                 // TODO: Email validation
-                if (!isset($uEmail) || empty($uEmail) || strtolower($uEmail) == 'null' ) {
+                if (!isset($uEmail) || empty($uEmail) || strtolower($uEmail) == 'null') {
                     continue;
                 }
 
-                // Generate username
+                // Generate username if it's not exists
                 if (!$uName || strtolower($uName) == 'null') {
                     $userService = $this->app->make(\Concrete\Core\Application\Service\User::class);
-                    $uName = $userService->generateUsernameFromEmail($_POST['uEmail']);
+                    $uName = $userService->generateUsernameFromEmail($this->request('uEmail'));
                 }
 
                 // Add user. Skip, if already exists
                 /** @var \Concrete\Core\User\UserInfo $ui */
-                $ui = \UserInfo::getByEmail($uEmail);
+                $ui = UserInfo::getByEmail($uEmail);
                 if (is_object($ui)) {
                     continue;
                 }
 
                 // Add user to database
                 $data = [
-                    'uName'     => $uName,
-                    'uEmail'    => $uEmail,
+                    'uName' => $uName,
+                    'uEmail' => $uEmail,
                 ];
-                /** @var \Concrete\Core\User\RegistrationService $userRegistrationService */
-                $userRegistrationService = Core::make('Concrete\Core\User\RegistrationServiceInterface');
-                $ui = $userRegistrationService->create($data);
+                /** @var RegistrationService $registrationService */
+                $registrationService = $this->app->make(RegistrationService::class);
+                $ui = $registrationService->create($data);
 
                 // Assign user group
                 if (isset($gName) && $gName) {
-
                     // Check if user has multiple group
                     if (strpos($gName, ',') !== false) {
                         $gNames = explode(',', $gName);
                         foreach ($gNames as $gName) {
-                            $group = \Group::getByName($gName);
+                            $group = Group::getByName($gName);
                             // Add group
                             if (!$group) {
-                                $group = \Group::add($gName, false);
+                                $group = Group::add($gName, false);
                             }
                             // Add user to the group
                             $user = $ui->getUserObject();
@@ -131,10 +130,10 @@ class ImportUserCsv extends \Concrete\Core\Page\Controller\DashboardPageControll
                             }
                         }
                     } else {
-                        $group = \Group::getByName($gName);
+                        $group = Group::getByName($gName);
                         // Add group
                         if (!$group) {
-                            $group = \Group::add($gName, false);
+                            $group = Group::add($gName, false);
                         }
                         // Add user to the group
                         $user = $ui->getUserObject();
@@ -151,7 +150,7 @@ class ImportUserCsv extends \Concrete\Core\Page\Controller\DashboardPageControll
                     $akHandles[] = $ak->getAttributeKeyHandle();
                 }
 
-                foreach ($columns as $handle => $column) {
+                foreach ($headers as $handle => $name) {
                     if (in_array($handle, $akHandles) && $$handle) {
                         $ui->setAttribute($handle, $$handle);
                     }
@@ -159,8 +158,56 @@ class ImportUserCsv extends \Concrete\Core\Page\Controller\DashboardPageControll
             }
             $this->redirect('/dashboard/system/backup/import_user_csv', 'imported');
         } else {
+            $this->set('error', $this->error);
             $this->view();
         }
+    }
+
+    protected function getHeaders()
+    {
+        // Static headers
+        // $headers = [t('Username'), t('Email'), t('Signup Date'), t('Status'), t('# Logins')];
+
+        $headers = [
+            'uID' => t('User ID'),
+            'uName' => t('User Name'),
+            'uEmail' => t('User Email'),
+            'uTimeZone' => t('Time Zone'),
+            'uDefaultLanguage' => t('Default Language'),
+            'uDateAdded' => t('Date Added'),
+            'uLastOnline' => t('Last Online'),
+            'uLastLogin' => t('Last Login'),
+            'uLastIP' => t('Last IP'),
+            'uPreviousLogin' => t('Previous Login'),
+            'uIsActive' => t('Is Active'),
+            'uIsValidated' => t('Is Validated'),
+            'uNumLogins' => t('Num of Logins'),
+            'gName' => 'User Group',
+        ];
+
+        foreach ($headers as $handle => $name) {
+            yield $handle => $name;
+        }
+
+        // Get headers for User attributes
+        $attributes = $this->getAttributeKeys();
+        foreach ($attributes as $attribute) {
+            yield $attribute->getAttributeKeyHandle() => $attribute->getAttributeKeyDisplayName();
+        }
+    }
+
+    /**
+     * Memoize the attribute keys so that we aren't looking them up over and over.
+     *
+     * @return array|Key[]
+     */
+    private function getAttributeKeys()
+    {
+        if (!isset($this->attributeKeys)) {
+            $this->attributeKeys = UserAttributeKey::getList();
+        }
+
+        return $this->attributeKeys;
     }
 
     public function imported()
@@ -169,21 +216,22 @@ class ImportUserCsv extends \Concrete\Core\Page\Controller\DashboardPageControll
         $this->view();
     }
 
-
     /**
-     * @return array
-     * Please add the columns here you want to import
-     * TODO: get from config file
+     * @param File|Version|\Concrete\Core\Entity\File\File $file
+     *
+     * @return Reader
      */
-    protected function getColumns() {
-        return $columns = [
-            'uName'         => 'Username',
-            'uEmail'        => 'User Email',
-            'uDisplayName'  => 'User Display Name',
-            'gName'         => 'User Group Name',
-            'firstname'     => 'First Name',
-            'phonic_name'   => 'Phonetic Name',
-            'zip_code'      => 'Zip Code'
-        ];
+    protected function getCsvReader($file)
+    {
+        ini_set('auto_detect_line_endings', true);
+        /** @var \Concrete\Flysystem\File $resource|\League\Flysystem\File */
+        $resource = $file->getFileResource();
+        if (method_exists($resource, 'readStream')) {
+            $reader = Reader::createFromStream($resource->readStream());
+        } else {
+            $reader = Reader::createFromString($resource->read());
+        }
+
+        return $reader;
     }
 }
--- csv_user_import_export/single_pages/dashboard/system/backup/import_user_csv.php
+++ csv_user_import_export/single_pages/dashboard/system/backup/import_user_csv.php
@@ -1,39 +1,41 @@
-<?php defined('C5_EXECUTE') or die("Access Denied."); ?>
+<?php defined('C5_EXECUTE') or die('Access Denied.'); ?>
 
-<?php if (isset($header) && is_array($header)) { ?>
+<?php if (isset($csvHeader) && is_array($csvHeader)) { ?>
 
-    <form method="post" action="<?=$view->action('import', $f->getFileID())?>">
-        <?=$this->controller->token->output('import')?>
+    <form method="post" action="<?=$view->action('csv_import', $fID); ?>">
+        <?=$this->controller->token->output('csv_import'); ?>
         <table class="table">
             <thead>
-                <tr>
-                    <th><?=t('Maps To');?></th>
-                    <th><?=t('CSV Header');?></th>
-                </tr>
+            <tr>
+                <th><?=t('CSV Header');?></th>
+                <th class="text-center"><?=tc('Direction', 'To');?></th>
+                <th><?=t('Maps To');?></th>
+            </tr>
             </thead>
             <tbody>
-                <?php foreach ($columns as $field => $column): ?>
-                    <tr>
-                        <td><?= $column ?></td>
-                        <td><?= $form->select($field, $header, false, ['default' => 'Please select']) ?></td>
-                    </tr>
-                <?php endforeach; ?>
+            <?php foreach ($headers as $handle => $name): ?>
+                <tr>
+                    <td><?= $form->select($handle, $csvHeader, $$handle) ?></td>
+                    <td class="text-center"><i class="fa fa-arrow-right" aria-hidden="true"></i></td>
+                    <td><?= h($name) ?> (<?= h($handle) ?>)</td>
+                </tr>
+            <?php endforeach; ?>
             </tbody>
         </table>
 
         <div class="ccm-dashboard-form-actions-wrapper">
             <div class="ccm-dashboard-form-actions">
-                <button class="pull-right btn btn-primary" type="submit" ><?=t('Import')?></button>
+                <button class="pull-right btn btn-primary" type="submit" ><?=t('Import'); ?></button>
             </div>
         </div>
     </form>
 
 <?php } else { ?>
 
-    <form method="post" action="<?=$view->action('select_mapping')?>">
-        <?=$this->controller->token->output('select_mapping')?>
+    <form method="post" action="<?=$view->action('select_mapping'); ?>">
+        <?=$this->controller->token->output('select_mapping'); ?>
         <fieldset>
-            <legend><?=t('Select CSV File')?></legend>
+            <legend><?=t('Select CSV File'); ?></legend>
             <div class="form-group">
                 <?php
                 /** @var \Concrete\Core\Application\Service\FileManager $html */
@@ -45,7 +47,7 @@
 
         <div class="ccm-dashboard-form-actions-wrapper">
             <div class="ccm-dashboard-form-actions">
-                <button class="pull-right btn btn-primary" type="submit" ><?=t('Next')?></button>
+                <button class="pull-right btn btn-primary" type="submit" ><?=t('Next'); ?></button>
             </div>
         </div>
     </form>
